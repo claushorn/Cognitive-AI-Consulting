b70ffc405c1aeacb4ee638a80c7b774dd7849213
claushorn <28705786+claushorn@users.noreply.github.com>
2025-05-08 17:17:41 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/strategic-ai-advisory/',
+  base: '/Cognitive-AI-Consulting/',
   server: {
     host: "::",
     port: 8080,
